ee04a1ef3fb967488fbd2f017b2d792f24306406
MasayaKataoka <ms.kataoka@gmail.com>
2021-10-19 14:54:47 +0900

    print yaml string
    Signed-off-by: MasayaKataoka <ms.kataoka@gmail.com>

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,6 +5,7 @@ CMAKE_ARGS=$2
 git config --global url."https://$GTHUB_TOKEN:x-oauth-basic@github.com/".insteadOf "https://github.com/"
 cd /home/ubuntu/Desktop/behavior_plugin_ws/src
 echo $REPOS_YAML > packages.repos
+cat REPOS_YAML
 vcs import . < packages.repos
 source /opt/ros/galactic/setup.bash
 source /home/ubuntu/Desktop/scenario_simulator_ws/install/local_setup.bash
