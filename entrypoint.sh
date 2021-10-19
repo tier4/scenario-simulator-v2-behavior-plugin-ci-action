a7500ccad09d0d82c5aea0a0f3308763be577e13
MasayaKataoka <ms.kataoka@gmail.com>
2021-10-19 18:24:35 +0900

    add test_image
    Signed-off-by: MasayaKataoka <ms.kataoka@gmail.com>

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,7 @@ REPOS_PATH=$2
 CMAKE_ARGS=$3
 ROS_DISTRO=$4
 
-cd /runtime_image
+cd /build_image
 touch entrypoint.sh
 echo "#!/bin/bash -l" >> entrypoint.sh
 echo "git config --global url.'https://$GTHUB_TOKEN:x-oauth-basic@github.com/'.insteadOf 'https://github.com/'" >> entrypoint.sh
@@ -19,10 +19,13 @@ echo "pip3 install colcon-lcov-result==0.5.0" >> entrypoint.sh
 echo "colcon mixin add default 'https://raw.githubusercontent.com/colcon/colcon-mixin-repository/1ddb69bedfd1f04c2f000e95452f7c24a4d6176b/index.yaml'" >> entrypoint.sh
 echo "colcon mixin update default" >> entrypoint.sh
 echo "source /home/ubuntu/Desktop/scenario_simulator_ws/install/local_setup.bash && colcon build --event-handlers console_cohesion+ --symlink-install --cmake-args ${CMAKE_ARGS}" >> entrypoint.sh
-#echo "source /home/ubuntu/Desktop/scenario_simulator_ws/install/local_setup.bash && colcon lcov-result --initial" >> entrypoint.sh
-#echo "source /home/ubuntu/Desktop/scenario_simulator_ws/install/local_setup.bash && colcon test --event-handlers console_cohesion+ --return-code-on-test-failure" >> entrypoint.sh
 
-docker build -t runtime_image \
+docker build -t build_image \
     --build-arg ROS_DISTRO="$ROS_DISTRO" \
     . \
-    && docker run runtime_image
+    && docker run build_image
+
+cd ../test_image
+docker build -t test_image \
+    . \
+    && docker run test_image
