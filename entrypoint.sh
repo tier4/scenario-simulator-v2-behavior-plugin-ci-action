203f13b6b9b48cda2ca102ea99b4fe8b898ff057
MasayaKataoka <ms.kataoka@gmail.com>
2021-10-19 21:28:46 +0900

    use mount option
    Signed-off-by: MasayaKataoka <ms.kataoka@gmail.com>

--- entrypoint.sh
+++ entrypoint.sh
@@ -26,7 +26,7 @@ cd /build_image
 docker build -t build_image \
     --build-arg ROS_DISTRO="$ROS_DISTRO" \
     . \
-    && docker run build_image -v /tmp/behavior_plugin_ws:/home/ubuntu/Desktop/behavior_plugin_ws
+    && docker run build_image --mount type=bind,source=/tmp/behavior_plugin_ws,target=/home/ubuntu/Desktop/behavior_plugin_ws
 
 echo =================================
 ls /tmp/behavior_plugin_ws
