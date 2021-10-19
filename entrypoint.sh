134e99ef592788d836a7490e803cecf7ffe52175
MasayaKataoka <ms.kataoka@gmail.com>
2021-10-19 15:11:27 +0900

    add REPOS_PATH argument
    Signed-off-by: MasayaKataoka <ms.kataoka@gmail.com>

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,15 +1,14 @@
 #!//bin/bash -l
-REPOS_YAML=$1
-CMAKE_ARGS=$2
+REPOSITORY_URL=$1
+REPOS_PATH=$2
+CMAKE_ARGS=$3
 
 git config --global url."https://$GTHUB_TOKEN:x-oauth-basic@github.com/".insteadOf "https://github.com/"
 cd /home/ubuntu/Desktop/behavior_plugin_ws/src
-echo $REPOS_YAML > packages.repos
-cat REPOS_YAML
-vcs import . < packages.repos
+git clone ${REPOSITORY_URL} target
 source /opt/ros/galactic/setup.bash
+vcs import /home/ubuntu/Desktop/behavior_plugin_ws/src < /home/ubuntu/Desktop/behavior_plugin_ws/src/target/${REPOS_PATH}
 source /home/ubuntu/Desktop/scenario_simulator_ws/install/local_setup.bash
-rosdep update
 rosdep install -iry --from-paths /home/ubuntu/Desktop/behavior_plugin_ws/src --rosdistro $ROS_DISTRO
 cd /home/ubuntu/Desktop/behavior_plugin_ws
 colcon build --symlink-install --cmake-args ${CMAKE_ARGS}
