550705b1bf5499b852775d5ffaf96b626d14e263
MasayaKataoka <ms.kataoka@gmail.com>
2021-10-19 16:19:32 +0900

    add source line
    Signed-off-by: MasayaKataoka <ms.kataoka@gmail.com>

--- entrypoint.sh
+++ entrypoint.sh
@@ -12,7 +12,6 @@ echo "cd /home/ubuntu/Desktop/behavior_plugin_ws/src" >> entrypoint.sh
 echo "git clone ${REPOSITORY_URL} target"  >> entrypoint.sh
 echo "source /opt/ros/$ROS_DISTRO/setup.bash" >> entrypoint.sh
 echo "vcs import /home/ubuntu/Desktop/behavior_plugin_ws/src < /home/ubuntu/Desktop/behavior_plugin_ws/src/target/${REPOS_PATH}" >> entrypoint.sh
-echo "source /home/ubuntu/Desktop/scenario_simulator_ws/install/local_setup.bash"
 echo "rosdep update" >> entrypoint.sh
 echo "rosdep install -iry --from-paths /home/ubuntu/Desktop/behavior_plugin_ws/src --rosdistro $ROS_DISTRO" >> entrypoint.sh
 echo "echo available packages" >> entrypoint.sh
@@ -22,9 +21,9 @@ echo "echo =============================================================" >> ent
 echo "cd /home/ubuntu/Desktop/behavior_plugin_ws" >> entrypoint.sh
 echo "colcon mixin add default 'https://raw.githubusercontent.com/colcon/colcon-mixin-repository/1ddb69bedfd1f04c2f000e95452f7c24a4d6176b/index.yaml'" >> entrypoint.sh
 echo "colcon mixin update default" >> entrypoint.sh
-echo "colcon build --event-handlers console_cohesion+ --symlink-install --cmake-args ${CMAKE_ARGS}" >> entrypoint.sh
-echo "colcon lcov-result --initial" >> entrypoint.sh
-echo "colcon test --event-handlers console_cohesion+ --return-code-on-test-failure" >> entrypoint.sh
+echo "source /home/ubuntu/Desktop/scenario_simulator_ws/install/local_setup.bash && colcon build --event-handlers console_cohesion+ --symlink-install --cmake-args ${CMAKE_ARGS}" >> entrypoint.sh
+echo "source /home/ubuntu/Desktop/scenario_simulator_ws/install/local_setup.bash && colcon lcov-result --initial" >> entrypoint.sh
+echo "source /home/ubuntu/Desktop/scenario_simulator_ws/install/local_setup.bash && colcon test --event-handlers console_cohesion+ --return-code-on-test-failure" >> entrypoint.sh
 
 docker build -t runtime_image \
     --build-arg ROS_DISTRO="$ROS_DISTRO" \
