d3755a5250b9d4ac1289f7b6b6784b3669efbee7
MasayaKataoka <ms.kataoka@gmail.com>
2021-10-19 14:27:41 +0900

    add repos_yaml argument
    Signed-off-by: MasayaKataoka <ms.kataoka@gmail.com>

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,11 @@
 #!//bin/bash -l
-REPOSITORY_URL=$1
+REPOS_YAML=$1
 CMAKE_ARGS=$2
 
 git config --global url."https://$GTHUB_TOKEN:x-oauth-basic@github.com/".insteadOf "https://github.com/"
 cd /home/ubuntu/Desktop/behavior_plugin_ws/src
-git clone ${REPOSITORY_URL}
+echo $REPOS_YAML > packages.repos
+vcs import . < packages.repos
 source /opt/ros/galactic/setup.bash
 source /home/ubuntu/Desktop/scenario_simulator_ws/install/local_setup.bash
 rosdep install -iry --from-paths /home/ubuntu/Desktop/behavior_plugin_ws/src --rosdistro $ROS_DISTRO
