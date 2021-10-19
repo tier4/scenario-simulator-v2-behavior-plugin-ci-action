d4669831dd989c2d8db24ae3e7031e88dfd4a229
MasayaKataoka <ms.kataoka@gmail.com>
2021-10-19 14:41:48 +0900

    update rosdep update line
    Signed-off-by: MasayaKataoka <ms.kataoka@gmail.com>

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,6 +8,7 @@ echo $REPOS_YAML > packages.repos
 vcs import . < packages.repos
 source /opt/ros/galactic/setup.bash
 source /home/ubuntu/Desktop/scenario_simulator_ws/install/local_setup.bash
+rosdep update
 rosdep install -iry --from-paths /home/ubuntu/Desktop/behavior_plugin_ws/src --rosdistro $ROS_DISTRO
 cd /home/ubuntu/Desktop/behavior_plugin_ws
 colcon build --symlink-install --cmake-args ${CMAKE_ARGS}
