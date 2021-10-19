37379d76b063a45be18f9d29fdf9931fa6085728
MasayaKataoka <ms.kataoka@gmail.com>
2021-10-19 14:16:38 +0900

    add build commands
    Signed-off-by: MasayaKataoka <ms.kataoka@gmail.com>

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,3 +7,6 @@ cd /home/ubuntu/Desktop/behavior_plugin_ws/src
 git clone ${REPOSITORY_URL}
 source /opt/ros/galactic/setup.bash
 source /home/ubuntu/Desktop/scenario_simulator_ws/install/local_setup.bash
+rosdep install -iry --from-paths /home/ubuntu/Desktop/behavior_plugin_ws/src --rosdistro $ROS_DISTRO
+cd /home/ubuntu/Desktop/behavior_plugin_ws
+colcon build --symlink-install --cmake-args ${CMAKE_ARGS}
