350c4ab68142c04a11482aa854c07eee90e26b8e
MasayaKataoka <ms.kataoka@gmail.com>
2021-10-20 14:21:14 +0900

    modify directory
    Signed-off-by: MasayaKataoka <ms.kataoka@gmail.com>

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,6 +7,7 @@ git config --global url.'https://$GTHUB_TOKEN:x-oauth-basic@github.com/'.instead
 cd /home/ubuntu/Desktop/behavior_plugin_ws/src
 git clone ${REPOSITORY_URL} target
 source /opt/ros/galactic/setup.bash
+source /home/ubuntu/Desktop/scenario_simulator_ws/install/local_setup.bash
 vcs import /home/ubuntu/Desktop/behavior_plugin_ws/src < /home/ubuntu/Desktop/behavior_plugin_ws/src/target/${REPOS_PATH}
 rosdep update
 rosdep install -iry --from-paths /home/ubuntu/Desktop/behavior_plugin_ws/src --rosdistro galactic
@@ -14,11 +15,11 @@ cd /home/ubuntu/Desktop/behavior_plugin_ws
 pip3 install colcon-lcov-result==0.5.0
 colcon mixin add default 'https://raw.githubusercontent.com/colcon/colcon-mixin-repository/1ddb69bedfd1f04c2f000e95452f7c24a4d6176b/index.yaml'
 colcon mixin update default
-apt-get install -y cowsay
+
 cowsay -f stegosaurus Build Step
-source /home/ubuntu/Desktop/scenario_simulator_ws/install/local_setup.bash && colcon build --event-handlers console_cohesion+ --symlink-install --cmake-args ${CMAKE_ARGS}
+colcon build --event-handlers console_cohesion+ --symlink-install --cmake-args ${CMAKE_ARGS}
 cowsay -f stegosaurus Initialize Lcov Step
-source /home/ubuntu/Desktop/scenario_simulator_ws/install/local_setup.bash && colcon lcov-result --initial
+colcon lcov-result --initial
 cowsay -f stegosaurus Test Step
-source /home/ubuntu/Desktop/scenario_simulator_ws/install/local_setup.bash && colcon test --event-handlers console_cohesion+ --return-code-on-test-failure
-source /home/ubuntu/Desktop/scenario_simulator_ws/install/local_setup.bash && colcon test-result --verbose
+colcon test --event-handlers console_cohesion+ --return-code-on-test-failure
+colcon test-result --verbose
