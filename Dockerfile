db0cae4ba2a545533371b11230314c4bd3ec50c8
MasayaKataoka <ms.kataoka@gmail.com>
2021-10-20 15:45:19 +0900

    add figlet
    Signed-off-by: MasayaKataoka <ms.kataoka@gmail.com>

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,7 @@
 FROM tier4/scenario_simulator_v2:galactic
 SHELL ["/bin/bash", "-c"]
 RUN mkdir -p /home/ubuntu/Desktop/behavior_plugin_ws/src
-RUN apt-get update && apt-get install -y cowsay
+RUN apt-get update && apt-get install -y cowsay figlet
 
 COPY entrypoint.sh /home/ubuntu/Desktop/entrypoint.sh
 RUN ["chmod", "+x", "/home/ubuntu/Desktop/entrypoint.sh"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -4,21 +4,29 @@ REPOS_PATH=$2
 CMAKE_ARGS=$3
 
 cd /home/ubuntu/Desktop/behavior_plugin_ws/src
+
+echo Clone ${REPOSITORY_NAME} Step | figlet | /usr/games/cowsay -f stegosaurus
 git clone https://$GITHUB_TOKEN:x-oauth-basic@github.com/${REPOSITORY_NAME}.git target
 source /opt/ros/galactic/setup.bash
 source /home/ubuntu/Desktop/scenario_simulator_ws/install/local_setup.bash
 vcs import /home/ubuntu/Desktop/behavior_plugin_ws/src < /home/ubuntu/Desktop/behavior_plugin_ws/src/target/${REPOS_PATH}
+
+echo Rosdep Step | figlet | /usr/games/cowsay -f stegosaurus
 rosdep update
 rosdep install -iry --from-paths /home/ubuntu/Desktop/behavior_plugin_ws/src --rosdistro galactic
 cd /home/ubuntu/Desktop/behavior_plugin_ws
+
+echo Install colcon-lcov Step | figlet | /usr/games/cowsay -f stegosaurus
 pip3 install colcon-lcov-result==0.5.0
 colcon mixin add default 'https://raw.githubusercontent.com/colcon/colcon-mixin-repository/1ddb69bedfd1f04c2f000e95452f7c24a4d6176b/index.yaml'
 colcon mixin update default
 
-/usr/games/cowsay -f stegosaurus Build Step
+echo Build Step | figlet | /usr/games/cowsay -f stegosaurus
 colcon build --event-handlers console_cohesion+ --symlink-install --cmake-args ${CMAKE_ARGS}
-/usr/games/cowsay -f stegosaurus Initialize Lcov Step
+
+echo Initialize Lcov Step | figlet | /usr/games/cowsay -f stegosaurus
 colcon lcov-result --initial
-/usr/games/cowsay -f stegosaurus Test Step
+
+echo Run Test Step | figlet | /usr/games/cowsay -f stegosaurus
 colcon test --event-handlers console_cohesion+ --return-code-on-test-failure
 colcon test-result --verbose
