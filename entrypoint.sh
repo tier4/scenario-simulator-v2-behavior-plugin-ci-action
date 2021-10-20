f52b36f4eea228d6954f6bc1332f87d70177cc06
MasayaKataoka <ms.kataoka@gmail.com>
2021-10-20 16:55:51 +0900

    specify galactic
    Signed-off-by: MasayaKataoka <ms.kataoka@gmail.com>

--- entrypoint.sh
+++ entrypoint.sh
@@ -43,6 +43,6 @@ echo "colcon test --event-handlers console_cohesion+ --return-code-on-test-failu
 echo "colcon test-result --verbose" >> entrypoint.sh
 
 docker build -t runtime_image \
-    --build-arg ROS_DISTRO=${ROS_DISTRO}
+    --build-arg ROS_DISTRO=galactic
     . \
     && docker run runtime_image
