99f10d2c3ae392443f512dede3ccda481a173e43
MasayaKataoka <ms.kataoka@gmail.com>
2021-10-20 15:24:42 +0900

    change authorization method
    Signed-off-by: MasayaKataoka <ms.kataoka@gmail.com>

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,10 @@
 #!//bin/bash -l
-REPOSITORY_URL=$1
+REPOSITORY_NAME=$1
 REPOS_PATH=$2
 CMAKE_ARGS=$3
 
-git config --global url.'https://$GITHUB_TOKEN:x-oauth-basic@github.com/'.insteadOf 'https://github.com/'
 cd /home/ubuntu/Desktop/behavior_plugin_ws/src
-git clone ${REPOSITORY_URL} target
+git clone https://$GITHUB_TOKEN:x-oauth-basic@github.com/${REPOSITORY_NAME}.git target
 source /opt/ros/galactic/setup.bash
 source /home/ubuntu/Desktop/scenario_simulator_ws/install/local_setup.bash
 vcs import /home/ubuntu/Desktop/behavior_plugin_ws/src < /home/ubuntu/Desktop/behavior_plugin_ws/src/target/${REPOS_PATH}
