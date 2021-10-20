17da1a7cfdc3a192d4ee4739e567d9d90f010a1f
MasayaKataoka <ms.kataoka@gmail.com>
2021-10-20 14:02:50 +0900

    hard code rosdistro name
    Signed-off-by: MasayaKataoka <ms.kataoka@gmail.com>

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,15 +2,14 @@
 REPOSITORY_URL=$1
 REPOS_PATH=$2
 CMAKE_ARGS=$3
-ROS_DISTRO=$4
 
 git config --global url.'https://$GTHUB_TOKEN:x-oauth-basic@github.com/'.insteadOf 'https://github.com/'
 cd /home/ubuntu/Desktop/behavior_plugin_ws/src
 git clone ${REPOSITORY_URL} target
-source /opt/ros/$ROS_DISTRO/setup.bash
+source /opt/ros/galactic/setup.bash
 vcs import /home/ubuntu/Desktop/behavior_plugin_ws/src < /home/ubuntu/Desktop/behavior_plugin_ws/src/target/${REPOS_PATH}
 rosdep update
-rosdep install -iry --from-paths /home/ubuntu/Desktop/behavior_plugin_ws/src --rosdistro $ROS_DISTRO
+rosdep install -iry --from-paths /home/ubuntu/Desktop/behavior_plugin_ws/src --rosdistro galactic
 cd /home/ubuntu/Desktop/behavior_plugin_ws
 pip3 install colcon-lcov-result==0.5.0
 colcon mixin add default 'https://raw.githubusercontent.com/colcon/colcon-mixin-repository/1ddb69bedfd1f04c2f000e95452f7c24a4d6176b/index.yaml'
